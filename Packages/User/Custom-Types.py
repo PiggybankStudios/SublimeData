c2251e5ac05ae9935567d5923fb348d78faed0af
Taylor Robbins <trobbins@hiddenbutler.com>
2017-04-23 10:04:49 -0700

    Initial commit

---
+++ Packages/User/Custom-Types.py
@@ -0,0 +1,264 @@
+import sublime
+import sublime_plugin
+import re
+import os
+import zipfile
+import tempfile
+
+def StringIsValidType(typeString):
+	if (typeString == None):
+		print("Empty string")
+		return False
+	
+	typeStringLength = len(typeString)
+	
+	searchResult = re.search("[A-Za-z0-9_]+", typeString)
+	
+	if (searchResult == None):
+		print("No match")
+		return False
+	
+	if (searchResult.start() != 0 or searchResult.end() != typeStringLength):
+		print("Not full string: " + 
+			"(" + str(searchResult.start()) + 
+			":" + str(searchResult.end()) + ")")
+		return False
+		
+	return True
+
+def UpdateZipArchiveFile(zipname, filename, data):
+	# generate a temp file
+	tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(zipname))
+	os.close(tmpfd)
+
+	# create a temp copy of the archive without filename            
+	with zipfile.ZipFile(zipname, 'r') as zin:
+		with zipfile.ZipFile(tmpname, 'w') as zout:
+			zout.comment = zin.comment # preserve the comment
+			for item in zin.infolist():
+				if item.filename != filename:
+					zout.writestr(item, zin.read(item.filename))
+
+	# replace with the temp archive
+	os.remove(zipname)
+	os.rename(tmpname, zipname)
+
+	# now add filename with its new data
+	with zipfile.ZipFile(zipname, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
+		zf.writestr(filename, data)
+		
+def GetZipArchiveFile(zipname, filename):
+	result = ""
+	with zipfile.ZipFile(zipname, 'r') as zin:
+		for item in zin.infolist():
+			if (item.filename == filename):
+				result = zin.read(item.filename)
+	
+	return result
+
+def GetCurrentSyntaxTypes(packagePath):
+	SyntaxFileName = packagePath + "\\User\\My C.sublime-syntax"
+	
+	# Read the current fileContents
+	file = open(SyntaxFileName, "r")
+	if (file == None):
+		print("Couldn't open syntax file for reading")
+		return None
+	fileContents = file.read()
+	file.close()
+	
+	searchResult = re.search("[\\s]+custom_types:[\\s]+'([A-Za-z0-9_\\|]+)'", fileContents)
+	
+	if (searchResult == None or
+		len(searchResult.groups()) < 1):
+		# TODO: Make this into an error window?
+		print("Couldn't find custom_types in sublime-syntax file")
+		return None
+	
+	typeListStart = searchResult.start(1)
+	typeListEnd = searchResult.end(1)
+	typeListStr = searchResult.group(1)
+	typeList = typeListStr.split("|")
+	
+	return typeList
+	
+def UpdateSyntaxTypes(packagePath, customTypes):
+	SyntaxFileName = packagePath + "\\User\\My C.sublime-syntax"
+	
+	# absPath = os.path.abspath(SyntaxFileName)
+	# print("absolute path is \"" + absPath + "\"")
+	
+	# Read the current fileContents
+	file = open(SyntaxFileName, "r")
+	if (file == None):
+		print("Couldn't open syntax file for reading")
+		return
+	fileContents = file.read()
+	file.close()
+	
+	# print("File is " + str(len(fileContents)) + " bytes long")
+	
+	searchResult = re.search("[\\s]+custom_types:[\\s]+'([A-Za-z0-9_\\|]?)'", fileContents)
+	
+	if (searchResult == None or
+		len(searchResult.groups()) < 1):
+		# TODO: Make this into an error window?
+		print("Couldn't find custom_types in sublime-syntax file")
+		return
+	
+	typeListStart = searchResult.start(1)
+	typeListEnd = searchResult.end(1)
+	typeListStr = searchResult.group(1)
+	
+	newTypeListStr = "|".join(customTypes)
+	
+	if (newTypeListStr == typeListStr):
+		print("Type List in syntax already matches")
+		return
+	
+	# Replace the old value in the file with our new list
+	fileContents = fileContents[:typeListStart] + newTypeListStr + fileContents[typeListEnd:]
+	
+	# Write the new fileContents
+	file = open(SyntaxFileName, 'w')
+	if (file == None):
+		print("Couldn't open syntax file for writing")
+		return;
+	file.write(fileContents)
+	file.close()
+	
+	print("Syntax type list updated successfully!")
+	
+
+class AddCustomTypeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		window = self.view.window()
+		
+		customTypes = []
+		projectSettings = window.project_data()
+		if (projectSettings == None):
+			print("No project opened")
+			return
+		# print("Current Project Settings: " + str(projectSettings))
+		
+		if (not "settings" in projectSettings):
+			projectSettings["settings"] = {}
+		if ("custom_types" in projectSettings["settings"]):
+			customTypes = projectSettings["settings"]["custom_types"]
+		
+		for region in self.view.sel():
+			newType = self.view.substr(region)
+			if (StringIsValidType(newType) and
+				not newType in customTypes):
+				print("Adding type \"" + newType + "\"")
+				customTypes.append(newType)
+			else:
+				print("Ignoring selection \"" + newType + "\"")
+		
+		projectSettings["settings"]["custom_types"] = customTypes
+		# print("New Project Settings: " + str(projectSettings))
+		
+		window.set_project_data(projectSettings)
+		
+		windowSettings = window.extract_variables()
+		while (windowSettings == None or
+			not "packages" in windowSettings):
+			# print("Could not get packages directory from window")
+			windowSettings = window.extract_variables()
+		# print("Window Settings: " + str(windowSettings))
+		
+		UpdateSyntaxTypes(windowSettings['packages'], customTypes)
+
+class RemoveCustomTypeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		window = self.view.window()
+		
+		customTypes = []
+		projectSettings = window.project_data()
+		if (projectSettings == None):
+			print("No project opened")
+			return
+		# print("Current Project Settings: " + str(projectSettings))
+		
+		if (not "settings" in projectSettings):
+			projectSettings["settings"] = {}
+		if ("custom_types" in projectSettings["settings"]):
+			customTypes = projectSettings["settings"]["custom_types"]
+		
+		for region in self.view.sel():
+			newType = self.view.substr(region)
+			if (StringIsValidType(newType) and
+				newType in customTypes):
+				print("Removing type \"" + newType + "\"")
+				customTypes.remove(newType)
+			else:
+				print("Ignoring selection \"" + newType + "\"")
+		
+		projectSettings["settings"]["custom_types"] = customTypes
+		# print("New Project Settings: " + str(projectSettings))
+		
+		window.set_project_data(projectSettings)
+		
+		windowSettings = window.extract_variables()
+		while (windowSettings == None or
+			not "packages" in windowSettings):
+			# print("Could not get packages directory from window")
+			windowSettings = window.extract_variables()
+		# print("Window Settings: " + str(windowSettings))
+		
+		UpdateSyntaxTypes(windowSettings['packages'], customTypes)
+
+class UpdateCustomTypesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		window = self.view.window()
+		
+		customTypes = []
+		projectSettings = window.project_data()
+		if (projectSettings != None and
+			"settings" in projectSettings and
+			"custom_types" in projectSettings["settings"]):
+			customTypes = projectSettings["settings"]["custom_types"]
+		
+		windowSettings = window.extract_variables()
+		while (windowSettings == None or
+			not "packages" in windowSettings):
+			# print("Could not get packages directory from window")
+			windowSettings = window.extract_variables()
+		# print("Window Settings: " + str(windowSettings))
+		
+		UpdateSyntaxTypes(windowSettings['packages'], customTypes)
+
+class ListCustomTypesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		window = self.view.window()
+		
+		projectSettings = window.project_data()
+		if (projectSettings == None):
+			print("No project opened")
+			return
+		# print("Current Project Settings: " + str(projectSettings))
+		
+		if ("settings" in projectSettings and
+			"custom_types" in projectSettings["settings"]):
+			projectTypes = projectSettings["settings"]["custom_types"]
+			print("Project Types: " + (", ".join(projectTypes)))
+		else:
+			print("No custom types in project settings")
+		
+		windowSettings = window.extract_variables()
+		while (windowSettings == None or
+			not "packages" in windowSettings):
+			# print("Could not get packages directory from window")
+			windowSettings = window.extract_variables()
+		# print("Window Settings: " + str(windowSettings))
+		
+		syntaxTypes = GetCurrentSyntaxTypes(windowSettings['packages'])
+		
+		if (not syntaxTypes == None):
+			print("Syntax Types: " + (", ".join(syntaxTypes)))
+		else:
+			print("Could not get syntax custom types")
+		
+		
+		
+		
---
+++ Packages/User/tutorial-plugin.py
@@ -0,0 +1,241 @@
+import sublime
+import sublime_plugin
+import re
+
+import webbrowser
+
+def GetCString(view, region):
+	startIndex = region.begin()
+	endIndex = region.end()
+	viewSize = view.size()
+	
+	if (startIndex == 0):
+		return "";
+	
+	# Step back till we find the (non-escaped) quotation mark or the beginning of line/file
+	currentChar = view.substr(startIndex-1)
+	while (startIndex > 0):
+		if (currentChar == '\"' and (startIndex <= 0 or view.substr(startIndex-2) != "\\")):
+			break
+		if (currentChar == '\n' or currentChar == '\r'):
+			# startIndex = startIndex + 1
+			return ""
+		if (currentChar == '<'):
+			break
+		if (endIndex - startIndex > 256):
+			break
+		# print("<" + currentChar)
+		startIndex = startIndex - 1
+		currentChar = view.substr(startIndex-1)
+	
+	if (startIndex == 0):
+		return "";
+	
+	# Step forward till we find the (non-escaped) quotation mark or the end of line/file
+	currentChar = view.substr(endIndex)
+	while (endIndex < viewSize):
+		if (currentChar == '\"' and (endIndex <= 0 or view.substr(endIndex-1) != "\\")):
+			break
+		if (currentChar == '\n' or currentChar == '\r'):
+			return ""
+		if (currentChar == '>'):
+			break;
+		if (endIndex - startIndex > 256):
+			break
+		# print(">" + currentChar)
+		endIndex = endIndex + 1
+		currentChar = view.substr(endIndex)
+	
+	if (endIndex >= viewSize):
+		return ""
+	
+	fileNameRegion = sublime.Region(startIndex, endIndex)
+	fileNameLength = endIndex - startIndex
+	fileName = view.substr(fileNameRegion)
+	# Enable this to escaped quotes into regular quotes in the result
+	# fileName = fileName.replace("\\\"", "\"")
+	
+	return fileName
+
+class PopupTestCommand(sublime_plugin.TextCommand):
+	popupItems = [
+	"Open String File", 
+	"Kill Word", 
+	"Pound Replace",
+	"Insert Hello World",
+	"Taylor Command",
+	"View Info",
+	"My Google Search",
+	"Update Custom Types",
+	"Custom Types Info"]
+	
+	commandItems = [
+	"open_string_file", 
+	"kill_word", 
+	"pound_replace",
+	"hello_world",
+	"taylor",
+	"view_info",
+	"my_google_search",
+	"update_custom_types",
+	"list_custom_types"]
+	
+	def popupDone(self, selectedIndex):
+		# print("Selected " + self.popupItems[selectedIndex] + "!")
+		print("Running command \"" + self.commandItems[selectedIndex] + "\"")
+		self.view.run_command(self.commandItems[selectedIndex])
+	
+	def run(self, edit):
+		self.view.show_popup_menu(self.popupItems, self.popupDone)
+
+#'Command' will be removed and all capital letters lowercase
+#multiple capital letters results in underscores being added
+class HelloWorldCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.insert(edit, 0, "Hello, World!")
+				
+class TaylorCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for selection in self.view.sel():
+			actualIndex = selection.begin()
+			for cIndex in range(selection.begin(), selection.end()):
+				char = self.view.substr(actualIndex)
+				print("\'" + char + "\'")
+				print(str(ord(char)+1))
+				replaceRange = sublime.Region(actualIndex, actualIndex+1)
+				newChar = chr(ord(char) + 5)
+				newLength = len(newChar)
+				self.view.replace(edit, replaceRange, newChar)
+				actualIndex += newLength
+			self.view.sel().add(sublime.Region(selection.begin(), actualIndex))
+
+class ViewInfoCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		
+		visibleRegion = view.visible_region()
+		print("Visible region: [{}, {}]".format(visibleRegion.begin(), visibleRegion.end()))
+
+class ViewPoundReplaceCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		visibleRegion = self.view.visible_region()
+		
+		for cIndex in range(visibleRegion.begin(), visibleRegion.end()):
+			charRegion = sublime.Region(cIndex, cIndex+1)
+			currentChar = self.view.substr(charRegion)
+			
+			if (currentChar != '\n' and currentChar != '\r' and
+				currentChar != '\t' and currentChar != ' '):
+				self.view.replace(edit, charRegion, '#')
+
+class PoundReplaceCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		
+		for region in self.view.sel():
+			
+			for cIndex in range(region.begin(), region.end()):
+				charRegion = sublime.Region(cIndex, cIndex+1)
+				currentChar = self.view.substr(charRegion)
+				
+				if (currentChar != '\n' and currentChar != '\r' and
+					currentChar != '\t' and currentChar != ' '):
+					self.view.replace(edit, charRegion, '#')
+
+# This is a simple command that deletes the word(s) your cursor is inside
+class KillWordCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			word = self.view.word(region)
+			self.view.erase(edit, word)
+
+class MyGoogleSearchCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# print("Running")
+		searchString = ""
+		for region in self.view.sel():
+			if (searchString != ""):
+				searchString = searchString + " "
+			searchString = searchString + self.view.substr(region)
+		
+		if (len(searchString) > 0):
+			print("My Google Search: Searching for \"" + searchString + "\"")
+			searchString = searchString.replace(" ", "+")
+			
+			webbrowser.open("https://www.google.com/search?q=" + searchString)
+		else:
+			print("Nothing selected to search for")
+
+def LineIsInclude(view, region):
+	matchRegex = "#include"
+	matchLength = len(matchRegex)
+	lineRegion = view.line(region)
+	line = view.substr(lineRegion)
+	
+	if (len(line) < len(matchRegex) or 
+		line[0:matchLength] != matchRegex):
+		return False
+	
+	fileName = GetCString(view, region)
+	fileNameLength = len(fileName)
+	
+	return len(fileName) > 0
+	
+
+# This is a simple function that opens the goto window
+# with the name of the #include header file that your
+# cursor is on already entered into the text box
+class OpenStringFileCommand(sublime_plugin.TextCommand):
+	
+	def want_event(self):
+		return True
+	
+	def run(self, edit, event = None, forcePos = None):
+		region = self.view.sel()[0]
+		# If we were passed an event we want to look at the point
+		# that was clicked on by the mouse instead of selected points
+		if (event != None):
+			textPos = self.view.window_to_text([event['x'], event['y']])
+			region = sublime.Region(textPos, textPos)
+		if (forcePos != None):
+			region = sublime.Region(forcePos, forcePos)
+		
+		fileName = GetCString(self.view, region)
+		
+		if (len(fileName) > 0):
+			print("Opening file overlay for filename \"" + fileName + "\"")
+			self.view.window().run_command("show_overlay", 
+				{"overlay": "goto", "show_files": True, "text": fileName})
+	
+	# This function determines whether this command shows up in the context menu
+	def is_visible(self, event = None):
+		region = self.view.sel()[0]
+		# If we were passed an event we want to look at the point
+		# that was clicked on by the mouse instead of selected points
+		if (event != None):
+			textPos = self.view.window_to_text([event['x'], event['y']])
+			region = sublime.Region(textPos, textPos)
+		
+		return LineIsInclude(self.view, region)
+
+class IncludeHoverEventListener(sublime_plugin.ViewEventListener):
+	
+	def OnPopupLinkClicked(self, link):
+		self.view.window().run_command("show_overlay", 
+			{"overlay": "goto", "show_files": True, "text": link})
+
+	def on_hover(self, point, hover_zone):
+		region = sublime.Region(point, point);
+		if (hover_zone == sublime.HOVER_TEXT and
+			LineIsInclude(self.view, region)):
+			# print("Line is #include!")
+			fileName = GetCString(self.view, region)
+			if (len(fileName) > 0):
+				htmlCode = ("<p style=\"padding:0px;margin:0px\">#included file:</p>" + 
+					"<a href=\"" + fileName +"\">" + 
+					fileName + "</a>?")
+				self.view.show_popup(htmlCode, 
+					sublime.HIDE_ON_MOUSE_MOVE_AWAY, point, 
+					100000, 10000, self.OnPopupLinkClicked)
+
+
+
