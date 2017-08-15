3df90cf8d7891bd05516d364760aa0f4bf9ab8f3
Taylor Robbins <trobbins@sure-fi.com>
2017-08-15 08:49:33 -0600

    Added defines to CreateFileHeaderCommand for header file's once inclusion

--- Packages/User/File-Header.py
+++ Packages/User/File-Header.py
@@ -4,34 +4,127 @@ import os
 import re
 import time
 
+def IsLower(char):
+	return ord(char) >= ord('a') and ord(char) <= ord('z')
+
+def IsUpper(char):
+	return ord(char) >= ord('A') and ord(char) <= ord('Z')
+
+def IsLetter(char):
+	return IsLower() or IsUpper()
+
+def IsNumber(char):
+	return ord(char) >= ord('0') and ord(char) <= ord('9')
+
+def GetNameParts(name):
+	parts = []
+	currIndex = 0
+	lastIndex = 0
+	lastCharWasUpper = False
+	lastCharWasNumber = False
+	while (currIndex < len(name)):
+		char = name[currIndex]
+		if (IsUpper(char) and not lastCharWasUpper):
+			if (currIndex > lastIndex):
+				newPart = name[lastIndex:currIndex].lower()
+				# print("Part: \"" + newPart + "\"")
+				parts.append(newPart)
+			lastIndex = currIndex
+		elif (IsNumber(char) and not lastCharWasNumber):
+			if (currIndex > lastIndex):
+				newPart = name[lastIndex:currIndex].lower()
+				# print("Part: \"" + newPart + "\"")
+				parts.append(newPart)
+			lastIndex = currIndex
+		elif (char == '_'):
+			if (currIndex > lastIndex):
+				newPart = name[lastIndex:currIndex].lower()
+				# print("Part: \"" + newPart + "\"")
+				parts.append(newPart)
+			lastIndex = currIndex+1
+		
+		lastCharWasUpper = (ord(char) >= ord('A') and ord(char) <= ord('Z'))
+		lastCharWasNumber = (ord(char) >= ord('0') and ord(char) <= ord('9'))
+		currIndex += 1
+	
+	if (currIndex > lastIndex):
+		newPart = name[lastIndex:currIndex].lower()
+		# print("Part: \"" + newPart + "\"")
+		parts.append(newPart)
+	
+	return parts
+
+def LineIsEmpty(line):
+	matchResult = re.search("^[\\t ]*$", line);
+	if (matchResult):
+		return True;
+	else:
+		return False;
+
 class CreateFileHeaderCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
+	def run(self, edit, headerExtensions=[".h", ".hpp"], sourceExtensions=[".c", ".cpp", ".py"], authorName="Taylor Robbins"):
 		filePath = self.view.file_name()
 		fileFolder, fileName = os.path.split(filePath)
-		fileNameNoExt, fileNameExt = os.path.splitext(fileName)
-		print("fileNameExt: " + fileNameExt)
+		fileNameNoExt, fileExtension = os.path.splitext(fileName)
+		print("fileExtension: " + fileExtension)
 		
-		authorName = "Taylor Robbins"
 		dateString = time.strftime("%m\\%d\\%Y")
+		
+		fileIsEmpty = True
+		lineNum = 0
+		cIndex = self.view.text_point(lineNum, 0)
+		while (cIndex < self.view.size()):
+			lineRegion = self.view.line(cIndex)
+			lineStr = self.view.substr(lineRegion)
+			if (lineStr != None and LineIsEmpty(lineStr) == False):
+				print("File is not empty")
+				fileIsEmpty = False
+				break
+			else:
+				lineNum += 1
+				cIndex = self.view.text_point(lineNum, 0)
+		
+		singleCommentStr = "//"
+		blockCommentStart = "/*"
+		blockCommentEnd = "*/"
 		
 		headerString = ""
 		selectRegion = False
 		newRegion = None
-		if (fileNameExt in [".h", ".hpp"]):
-			headerString += "/*\n"
+		if (fileExtension in headerExtensions):
+			headerString += blockCommentStart + "\n"
 			headerString += "File:   " + fileName   + "\n"
 			headerString += "Author: " + authorName + "\n"
 			headerString += "Date:   " + dateString + "\n"
-			headerString += "*/\n"
+			headerString += blockCommentEnd + "\n"
 			headerString += "\n"
-		elif (fileNameExt in [".c", ".cpp", ".py"]):
-			headerString += "/*\n"
+			
+			if (fileIsEmpty):
+				nameParts = GetNameParts(fileNameNoExt)
+				print("Name Parts: " + str(nameParts))
+				
+				defineName = ""
+				for part in nameParts:
+					defineName += "_" + part.upper()
+				defineName += "_" + fileExtension[1:].upper()
+				
+				headerString += "#ifndef " + defineName + "\n"
+				headerString += "#define " + defineName + "\n"
+				headerString += "\n"
+				selectIndex = len(headerString)
+				selectRegion = True
+				newRegion = sublime.Region(selectIndex, selectIndex)
+				headerString += "\n\n"
+				headerString += "#endif " + singleCommentStr + " " + defineName + "\n"
+		
+		elif (fileExtension in sourceExtensions):
+			headerString += blockCommentStart + "\n"
 			headerString += "File:   " + fileName   + "\n"
 			headerString += "Author: " + authorName + "\n"
 			headerString += "Date:   " + dateString + "\n"
 			headerString += "Description: \n"
 			headerString += "\t** None \n"
-			headerString += "*/\n\n"
+			headerString += blockCommentEnd + "\n\n"
 			searchResult = re.search("\t\*\* (None)", headerString)
 			newRegion = sublime.Region(searchResult.start(1),searchResult.end(1))
 			selectRegion = True
