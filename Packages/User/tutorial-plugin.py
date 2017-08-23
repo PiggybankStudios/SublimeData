9f43a7ee4f6fe200eb1dcb889e51f6e119f6381f
Taylor Robbins <trobbins@sure-fi.com>
2017-08-23 10:02:15 -0600

    Moved grab_next bindings to alt+s and find bindings back to ctrl+f. ctrl+d is now bound to a new command goto_next_scope which allows you to easily jump foward and back through function declarations and My C headers.

--- Packages/User/tutorial-plugin.py
+++ Packages/User/tutorial-plugin.py
@@ -561,6 +561,104 @@ class AsciiToHexCommand(sublime_plugin.TextCommand):
 				
 				self.view.replace(edit, region, replaceStr)
 
+def SplitScopesString(scopeString):
+	scopes = []
+	
+	parts = scopeString.split(" ")
+	for part in parts:
+		scopeName = part.strip()
+		if (len(scopeName) == 0):
+			continue
+		# print("Scope: \"" + scopeName + "\"")
+		scopes.append(scopeName)
+	
+	return scopes
+
+class GotoNextScopeCommand(sublime_plugin.TextCommand):
+	def run(self, edit, forward=True, loop_around=False, scope_match="entity.", show_only=False, select_whole=True, show_centered=True):
+		scopeMatchLen = len(scope_match)
+		
+		selections = self.view.sel()
+		if (len(selections) != 1):
+			print("More than one selection. Cannot goto next")
+		
+		startIndex = selections[0].b
+		cIndex = startIndex
+		
+		success = False
+		foundNonMatch = False
+		foundFirst = False
+		firstIndex = 0
+		lastIndex = 0
+		while (True):
+			scopes = SplitScopesString(self.view.scope_name(cIndex))
+			# print("Scopes: " + str(scopes))
+			
+			isMatch = False
+			for scope in scopes:
+				if (len(scope) >= scopeMatchLen and scope[0:scopeMatchLen] == scope_match):
+					isMatch = True
+					break
+			
+			if (isMatch):
+				if (foundNonMatch and foundFirst == False):
+					foundFirst = True
+					firstIndex = cIndex
+					if (forward == False):
+						firstIndex += 1
+						if (loop_around and firstIndex >= self.view.size()): firstIndex = 0
+					print("Start Find: %u" % firstIndex)
+			else:
+				foundNonMatch = True
+				if (foundFirst):
+					lastIndex = cIndex
+					if (forward == False):
+						lastIndex += 1
+						if (loop_around and lastIndex >= self.view.size()): lastIndex = 0
+					print("End find: %u" % lastIndex)
+					success = True
+					break
+			
+			if (forward):
+				cIndex += 1
+				if (cIndex >= self.view.size()):
+					if (loop_around): cIndex = 0
+					else: break
+			else:
+				cIndex -= 1
+				if (cIndex < 0):
+					if (loop_around): cIndex = self.view.size()-1
+					else: break
+			
+			if (cIndex == startIndex):
+				break
+		
+		# NOTE: If the match extends all the way to the end of the file
+		#		then we need to handle that case here
+		if (foundFirst and success == False):
+			if (cIndex < 0):
+				lastIndex = 0
+				success = True
+			elif (cIndex >= self.view.size()):
+				lastIndex = self.view.size()
+				success = True
+			else:
+				print("Not sure exactly how we got here...")
+		
+		newRegion = sublime.Region(min(firstIndex, lastIndex), min(firstIndex, lastIndex))
+		if (select_whole):
+			newRegion = sublime.Region(max(firstIndex, lastIndex), min(firstIndex, lastIndex))
+		if (success):
+			if (show_centered):
+				self.view.show_at_center(newRegion.b)
+			else:
+				self.view.show(newRegion.b)
+			if (show_only == False):
+				self.view.sel().clear()
+				self.view.sel().add(newRegion)
+		else:
+			print("No Entities Found")
+
 # class ShowPositionCommand(sublime_plugin.TextCommand):
 # 	def run(self, edit, position, buffer_size=5):
 # 		row, col = self.view.rowcol(position)
