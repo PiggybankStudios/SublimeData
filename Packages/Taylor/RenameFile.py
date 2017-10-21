b82a080bf8e060cd63e9e98a43c8719fd2a212dd
Taylor Robbins <trobbins@hiddenbutler.com>
2017-10-20 17:52:21 -0700

    Moved around some key bindings so KillLineCommand is now bound to ctrl+r. Moved some bindings for switching between panes. Added RenameFileCommand and bound it to ctrl+alt+r. ctrl+k is currently unbound now. jump_forward and jump_back are now bound to ctrl+< and ctrl+>.

---
+++ Packages/Taylor/RenameFile.py
@@ -0,0 +1,55 @@
+
+import sublime
+import sublime_plugin
+import os
+import functools
+
+
+class RenameFileCommand(sublime_plugin.TextCommand):
+#
+	def run(self, edit):
+	#
+		filename = self.view.file_name()
+		branch, leaf = os.path.split(filename)
+		
+		if not os.access(filename, os.W_OK):
+		#
+			sublime.error_message(leaf + " is read-only")
+		#
+		
+		panel = self.view.window().show_input_panel("New Name:", leaf, functools.partial(self.on_done, filename, branch), None, None)
+		
+		name, ext = os.path.splitext(leaf)
+		panel.sel().clear()
+		panel.sel().add(sublime.Region(0, len(name)))
+	#
+	
+	def on_done(self, oldFileName, branch, leaf):
+	#
+		newFileName = os.path.join(branch, leaf)
+		
+		try:
+		#
+			if (len(leaf) == 0):
+			#
+				sublime.error_message("No filename given")
+				return;
+			#
+				
+			if (os.path.exists(newFileName) and oldFileName != newFileName):
+			#
+				sublime.error_message(newFileName + " already exists")
+				return;
+			#
+				
+			os.rename(oldFileName, newFileName)
+			
+			v = self.view.window().find_open_file(oldFileName)
+			if v: v.retarget(newFileName)
+		#
+		except Exception as e:
+		#
+			sublime.status_message("Unable to rename: " + str(e))
+		#
+	#
+#
