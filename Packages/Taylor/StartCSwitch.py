88148fba0ec23d4e025b8d1b94b1bf410dac37b3
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-01-03 01:05:56 +0000

    Got the "StartCSwitchCommand" working on Linux (cswitch needs to be copied into "/usr/bin" or similar in order to be found)

--- Packages/Taylor/StartCSwitch.py
+++ Packages/Taylor/StartCSwitch.py
@@ -1,4 +1,4 @@
-import os, sys, sublime, sublime_plugin
+import os, platform, sys, sublime, sublime_plugin
 from Taylor.Functions import *
 import subprocess
 
@@ -6,16 +6,22 @@ class StartCswitchCommand(sublime_plugin.TextCommand):
 #
 	def run(self, edit):
 	#
-		fileName = self.view.file_name()
+		fileName = self.view.file_name();
 		if (fileName == ""):
 		#
-			print("Current tab doesn't represent a file on disk!")
-			return
+			print("Current tab doesn't represent a file on disk!");
+			return;
 		#
 		
 		# print("Running on " + os.name)
-		print("Starting CSwitch on \"%s\"!" % (fileName))
-		subprocess.Popen(["cswitch.exe", fileName, "-top", "-size=(220,350)"]);
+		print("Starting CSwitch on \"%s\"!" % (fileName));
+		print("PATH = %s!" % (os.environ.get("PATH")));
+		cswitchBinaryName = "cswitch";
+		if (platform.system() == "Windows"):
+		#
+			cswitchBinaryName = "cswitch.exe";
+		#
+		subprocess.Popen([cswitchBinaryName, fileName, "-top", "-size=(220,350)"]);
 
 	#
 #
